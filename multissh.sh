e34b1e9a52eadc12abf7d8a7503fef670421821f
Abhijit Kane <akane@walmartlabs.com>
2014-03-10 16:34:42 +0530

    Updated to handle wrong host / wrong credentials

--- multissh.sh
+++ multissh.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/expect
 #usage - [script] hostFile userName Password commandFile
+#---------CONFIG------------
+set PASSWORD_PROMPT_PATTERN "*?assword"
+set SHELL_PATTERN "-bash-4.1$ "
+
+
 set timeout 2
 
 set f1 [open [lindex $argv 3]]
@@ -18,26 +23,36 @@ close $fd
 set fd [open $output "a"]
 
 proc check_host {hostname} {
+    global PASSWORD_PROMPT_PATTERN SHELL_PATTERN
     global user password fd commands
     spawn ssh $user@$hostname
     sleep 5
-    expect "*yes/no" { 
-    	send "yes\n"
-    	expect "*?assword" { send "$password\n" }
-    } 
-    expect "*?assword" { send "$password\n" }
 
-    expect -re "-bash-4.1$ "
+    expect {
+        eof { puts "\nSSH failure for $hostname\n"; exit 1 }
+        "*yes/no" { 
+            send "yes\n";
+            expect $PASSWORD_PROMPT_PATTERN { send "$password\n" }
+        } 
+    }
+
+    expect $PASSWORD_PROMPT_PATTERN { send "$password\n" }
+
+    expect {
+        $PASSWORD_PROMPT_PATTERN { puts "\nIncorrect credentials.\n";exit 1}
+        -re $SHELL_PATTERN
+    }
+
     sleep 1
 
 
     foreach command $commands {
-    	send "$command\n"
-    	expect -re {.*} {}
-    	sleep 1
-    	expect "*\n"
-    	puts $fd "From $hostname:\n $expect_out(0,string)"
-    	expect -re {.*} {}
+        send "$command\n"
+        expect -re {.*} {}
+        sleep 1
+        expect "*\n"
+        puts $fd "From $hostname:\n $expect_out(0,string)"
+        expect -re {.*} {}
     }
  
     send "logout\r"
